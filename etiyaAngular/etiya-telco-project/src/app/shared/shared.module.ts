8764739e48f1f8b9a5eb4527d1803fb3fd561afb
mirzacckc <cicekcimirzacan@gmail.com>
2022-08-18 16:45:58 +0300

    create etiya-telco-project

---
+++ etiyaAngular/etiya-telco-project/src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OverlayTitleComponent } from './components/overlay-title/overlay-title.component';
+
+
+
+@NgModule({
+  declarations: [
+    OverlayTitleComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    OverlayTitleComponent
+    
+  ]
+})
+export class SharedModule { }
