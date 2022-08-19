1a5dcfa2fe474baf833a9113b434788a05144e45
mirzacckc <cicekcimirzacan@gmail.com>
2022-08-19 14:35:30 +0300

    add settings menu component

--- etiyaAngular/etiya-telco-project/src/app/shared/shared.module.ts
+++ etiyaAngular/etiya-telco-project/src/app/shared/shared.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OverlayTitleComponent } from './components/overlay-title/overlay-title.component';
 import { SideFilterComponent } from './components/side-filter/side-filter.component';
+import { SettingsMenuComponent } from './components/settings-menu/settings-menu.component';
 
 
 
 @NgModule({
   declarations: [
     OverlayTitleComponent,
-    SideFilterComponent
+    SideFilterComponent,
+    SettingsMenuComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
     OverlayTitleComponent,
-    SideFilterComponent    
+    SideFilterComponent,
+    SettingsMenuComponent
   ]
 })
 export class SharedModule { }
