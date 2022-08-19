f2c849cefcdc350919fe67ca68c21bb02a2915fb
mirzacckc <cicekcimirzacan@gmail.com>
2022-08-19 16:56:39 +0300

    Update

--- etiyaAngular/etiya-telco-project/src/app/shared/shared.module.ts
+++ etiyaAngular/etiya-telco-project/src/app/shared/shared.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { OverlayTitleComponent } from './components/overlay-title/overlay-title.component';
 import { SideFilterComponent } from './components/side-filter/side-filter.component';
 import { SettingsMenuComponent } from './components/settings-menu/settings-menu.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
+import { TableInfoComponent } from './components/table-info/table-info.component';
 
 
 
@@ -10,7 +13,10 @@ import { SettingsMenuComponent } from './components/settings-menu/settings-menu.
   declarations: [
     OverlayTitleComponent,
     SideFilterComponent,
-    SettingsMenuComponent
+    SettingsMenuComponent,
+    NavbarComponent,
+    UserInfoComponent,
+    TableInfoComponent
   ],
   imports: [
     CommonModule
@@ -18,7 +24,10 @@ import { SettingsMenuComponent } from './components/settings-menu/settings-menu.
   exports:[
     OverlayTitleComponent,
     SideFilterComponent,
-    SettingsMenuComponent
+    SettingsMenuComponent,
+    NavbarComponent,
+    UserInfoComponent,
+    TableInfoComponent
   ]
 })
 export class SharedModule { }
