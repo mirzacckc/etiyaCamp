4d04953fb3da1b18d0c41c00cacd92800d168da7
mirzacckc <cicekcimirzacan@gmail.com>
2022-08-22 14:12:09 +0300

    update warning message

--- etiyaAngular/etiya-telco-project/src/app/shared/components/warning-message/warning-message.component.ts
+++ etiyaAngular/etiya-telco-project/src/app/shared/components/warning-message/warning-message.component.ts
@@ -7,6 +7,8 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class WarningMessageComponent implements OnInit {
   @Input() warnText!:string;
+  @Input() warnAlert!:boolean;
+  @Input() warnIcon!:string;
   constructor() { }
 
   ngOnInit(): void {
